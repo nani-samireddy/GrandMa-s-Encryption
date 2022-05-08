e7fd4bf6c5add80d093f7de2598f6da3e8fc0b92
nani-samireddy <nanisamireddy05@gmail.com>
2022-05-08 19:38:54 +0530

    started the front-end

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function encode(params) {
+    
+}
+
+
+function decode(params) {
+    
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        vampire_black: "rgba(9, 7, 10, 1)",
+        pale_silver: "rgba(197, 190, 180, 1)",
+        cinereous: "rgba(156, 137, 123, 1)",
+      },
+      fontFamily: {
+        inter: "'Inter', sans-serif",
+        poppins: "'Poppins', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
