f6a8fa52f723027a3cbcd0f5cf0f74e202475657
nani-samireddy <nanisamireddy05@gmail.com>
2022-05-13 16:37:13 +0530

    Adjusted input overflow

--- public/script.js
+++ public/script.js
@@ -1,18 +1,46 @@
 const encryptionButton = document.getElementById("EncryptionButton");
 const decryptionButton = document.getElementById("DecryptionButton");
+const copyButtonForEncryption = document.getElementById(
+  "CopyButtonForEncryption"
+);
+const copyButtonForDecryption = document.getElementById(
+  "CopyButtonForDecryption"
+);
 
-encryptionButton.addEventListener('click',)
+const messageInputTag = document.getElementById("messageInputTag");
+const messageOutputTag = document.getElementById("decryptedMessageCodeOutput");
+const grandmaCodeOutputTag = document.getElementById(
+  "encryptedMessageCodeOutput"
+);
+const grandmaCodeInputTag = document.getElementById("grandmaCodeInputTag");
 
-function encode(params) {
+//Event listeners
+encryptionButton.addEventListener("click", encryptAndEnableCopy);
 
+decryptionButton.addEventListener("click", decryptAndEnableCopy);
+
+copyButtonForEncryption.addEventListener("click", () => {
+  console.log("CopyButtonForEncryption pressed");
+});
+
+copyButtonForDecryption.addEventListener("click", () => {
+  console.log("CopyButtonForDecryption pressed");
+});
+
+//functions
+function encode(message) {
+  return message;
 }
 
-
-function decode(params) {
-    
+function decode(grandmaCode) {
+  return grandmaCode;
 }
 
-
-function CreateSecretCode(params) {
-    
+function encryptAndEnableCopy() {
+  let message = messageInputTag.value;
+  message = message.toLowerCase();
+  let grandMaCode = encode(message);
+  grandmaCodeOutputTag.value = grandMaCode;
 }
+
+function decryptAndEnableCopy() {}
