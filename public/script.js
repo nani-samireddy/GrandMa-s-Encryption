535d0dd6c6a57839ae4f30634bea26b7460cc2b2
nani-samireddy <nanisamireddy05@gmail.com>
2022-05-13 15:42:27 +0530

    finished ui for mobile

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
+const encryptionButton = document.getElementById("EncryptionButton");
+const decryptionButton = document.getElementById("DecryptionButton");
+
+encryptionButton.addEventListener('click',)
+
 function encode(params) {
-    
+
 }
 
 
 function decode(params) {
+    
+}
+
+
+function CreateSecretCode(params) {
     
 }
