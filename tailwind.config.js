6488e00794a823b48defd505371a4494d3787f4a
nani-samireddy <nanisamireddy05@gmail.com>
2022-05-13 14:57:02 +0530

    made some minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         vampire_black: "rgba(9, 7, 10, 1)",
         pale_silver: "rgba(197, 190, 180, 1)",
         cinereous: "rgba(156, 137, 123, 1)",
+        bright_grey: "rgba(237, 236, 236, 1)",
+        lemon_lime: "rgba(149, 191, 30, 1)",
+        Interdimensional_Blue: "rgba(72, 30, 191, 1)",
+        Philippine_Brown: "rgba(94, 23, 20, 1)",
       },
       fontFamily: {
         inter: "'Inter', sans-serif",
